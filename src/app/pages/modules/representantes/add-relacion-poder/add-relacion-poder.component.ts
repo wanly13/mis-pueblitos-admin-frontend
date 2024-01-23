0b0632e849a10bc9420d3d2941d4ba3d011237d8
wanly13 <wanlyobregon13@gmail.com>
2024-01-22 21:21:16 -0500

    archivo

--- src/app/pages/modules/representantes/add-relacion-poder/add-relacion-poder.component.ts
+++ src/app/pages/modules/representantes/add-relacion-poder/add-relacion-poder.component.ts
@@ -22,10 +22,10 @@ export class AddRelacionPoderComponent {
     private loadingService: LoadingService,
     private fb: FormBuilder,
     private representantesService: RepresentantesService,
-    private businessService : BusinessService,
+    private businessService: BusinessService,
   ) {
     this.addValueForm = this.fb.group({
-      archivo: [{ value: null, disabled: false }],
+      
       documentoIdentidad: [{ value: null, disabled: false }],
       idEstadoPoder: [{ value: null, disabled: false }],
       idPoder: [{ value: null, disabled: false }],
@@ -41,6 +41,7 @@ export class AddRelacionPoderComponent {
     this.general_loads();
     console.log("TypeModal", this.TypeModal);
     this.addValueForm.patchValue(this.TypeModal.data)
+    this.load_oficinas_empresa(this.addValueForm.value.taxIdEmpresa)
   }
 
 
@@ -56,7 +57,8 @@ export class AddRelacionPoderComponent {
     this.loadingService.show()
     if (this.TypeModal.type == 'CREATE') {
       console.log();
-
+      var data = this.addValueForm.value
+      data.archivo = this.ArchivosCargados
       this.representantesService.create_relacion_poder(this.addValueForm.value).subscribe(
         (response: any) => {
           Swal.fire({
@@ -99,9 +101,64 @@ export class AddRelacionPoderComponent {
         }
       );
     }
+    console.log("LISTA IMAGENES: ", this.ArchivosCargados);
+
   }
 
+  // ------------------ IMPLEMENTACION DE ARCHIVOS ---------------------- \\
+  ArchivosCargados: any[] = [];
 
+  onFileSelected(event: any): void {
+    const files: FileList = event.target.files;
+
+    for (let i = 0; i < files.length; i++) {
+      const file: File = files.item(i);
+
+      // Lee el archivo como base64
+      this.readAndAddFile(file);
+    }
+  }
+
+  readAndAddFile(file: File): void {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      // Agrega el archivo a la lista
+      this.ArchivosCargados.push({
+        nombreArchivo: this.generateUniqueFileName(),
+        imagen: e.target.result,
+        fechaCarga: new Date(),
+        tipo: file.type,
+      });
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+  generateUniqueFileName(): string {
+    const now = new Date();
+    return `ARCHIVE${now.getTime()}`;
+  }
+
+  deleteImage(item: any) {
+    const index = this.ArchivosCargados.findIndex((archivo) => archivo.nombreArchivo === item.nombreArchivo);
+
+    // Verifica si se encontró el elemento en la lista
+    if (index !== -1) {
+      // Elimina el elemento de la lista usando splice
+      this.ArchivosCargados.splice(index, 1);
+    } else {
+      console.error(`Elemento con nombre ${item.nombreArchivo} no encontrado en la lista.`);
+    }
+  }
+  imagenSeleccionada: any = null;
+  viewImage(item: any) {
+    this.imagenSeleccionada = item;
+  }
+  closeImageView(): void {
+    // Cierra la vista de la imagen
+    this.imagenSeleccionada = null;
+  }
 
   general_loads() {
     this.load_empresas();
@@ -182,7 +239,7 @@ export class AddRelacionPoderComponent {
     );
   }
   list_oficinas: any[] = [];
-  load_oficinas_empresa(value : any) {
+  load_oficinas_empresa(value: any) {
     this.loadingService.show();
     this.list_oficinas = []
     this.businessService.get_oficinas_empresa(value).subscribe(
@@ -195,6 +252,8 @@ export class AddRelacionPoderComponent {
       }
     );
   }
+
+
 }
 
 export class InputModal {
